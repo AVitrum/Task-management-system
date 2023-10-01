5d5024de523c240a4dcc415090ebb370a661bcc0
Orekusandoru <sasha.g360@gmail.com>
2023-10-01 17:51:31 +0300

    added authentication

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,7 +1,40 @@
+import { useContext, useEffect } from 'react';
+import { UserContext } from './UserContext';
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
 export default function Header(){
+
+    const {token, setUserInfo, userInfo, setToken } = useContext(UserContext);
+    const navigate = useNavigate();
+    
+    const profile = async () => {
+    const res = await axios.get('http://16.171.232.56:8080/api/v1/auth/profile', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        setUserInfo(res.data);
+    
+    };
+
+    const logout = () => {
+        Cookies.remove('userInfo');
+        Cookies.remove('token');
+        setUserInfo({ id: -1, username: '', email: '', role: '' });
+        setToken('');
+        navigate('/');
+        window.location.reload();
+    };
+
+    useEffect(() => {
+        profile();
+    }, []);
+
     return(
        <>
-        Header
+        <button className="btnlog" onClick={logout}>Logout</button>
        </>
     );
 }
--- front/src/pages/LoginPage.tsx
+++ front/src/pages/LoginPage.tsx
@@ -1,7 +1,47 @@
+import axios from "axios";
+import { useState, useContext } from "react";
+import { UserContext } from "../components/UserContext";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage(){
-    return(
-       <>
-        LoginPage
-       </>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const {setToken} = useContext(UserContext);
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (e:React.FormEvent) => {
+        e.preventDefault();
+        const res = await axios.post('http://16.171.232.56:8080/api/v1/auth/authenticate',
+        {
+            username,     
+            password,
+        })
+        setToken(res.data.token);
+        navigate('/');
+        window.location.reload();
+    }
+
+    return (
+        <div>
+            <form className="register" onSubmit={(e)=>onSubmit(e)}>
+                <h1>Login</h1>
+                <input type="text"
+                    placeholder="username"
+                    value={username}
+                    onChange={ev => setUsername(ev.target.value)}
+                    className="login-input" />
+                
+                <input type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)}
+                    className="login-input" />
+                <button className="btnlog" type="submit">Login</button>
+            </form>
+        </div>
+
     );
+
+
 }
